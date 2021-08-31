43bd3e8361848f4e01692e6b4524d23ba15436ee
Albert Dominguez <albert.j.dominguez21@gmail.com>
2021-08-31 10:44:46 -0500

    refactor

--- js/movies-application.js
+++ js/movies-application.js
@@ -1,14 +1,10 @@
 "use strict";
 fetch('https://obtainable-scrawny-cactus.glitch.me/movies').then( response => {
-    response.json().then( movies => {
-
-         var html = "";
-        movies.forEach( movie  => {
+    response.json().then(movies => {
+        var html = "";
+        movies.forEach(movie => {
             html+= movie.title
         });
         $(".loading").html(html)
-            // do something with the username login
-            // console.log(movies);
-        // });
     });
 });
