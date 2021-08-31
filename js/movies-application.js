9038b79edc02b9bfa519c82c5701ebd20f210ce8
Orenthal Hill <ojhill98@gmail.com>
2021-08-31 10:18:37 -0500

    loading done

--- js/movies-application.js
+++ js/movies-application.js
@@ -2,9 +2,13 @@
 fetch('https://obtainable-scrawny-cactus.glitch.me/movies').then( response => {
     response.json().then( movies => {
 
-        // users.forEach( userObj  => {
+         var html = "";
+        movies.forEach( movie  => {
+            html+= movie.title
+        });
+        $(".loading").html(html)
             // do something with the username login
-            console.log(movies);
+            // console.log(movies);
         // });
     });
 });
